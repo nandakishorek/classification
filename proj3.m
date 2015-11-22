c2ecce37a1d0e4d62123880bdec566f1757ea5eb
nandakishore.k <nandakishorek17@gmail.com>
2015-11-22 16:08:39 -0500

    NN feed forward completed

--- proj3.m
+++ proj3.m
@@ -37,13 +37,13 @@ lgr_error = zeros(1, length(images));
 
 % gradient descent
 for i = 1 : length(images)
-    ak = Wlr' * images(:, i) + blr';
+    a = Wlr' * images(:, i) + blr';
     
     y = zeros(k, 1);
-    exp_ak = exp(ak);
-    sigma_ak = sum(exp(ak));
-    for j = 1 : k
-        y(j, 1) = exp_ak(j, 1) / sigma_ak;
+    exp_a = exp(a);
+    sigma_a = sum(exp(a));
+    for m = 1 : k
+        y(m, 1) = exp_a(m, 1) / sigma_a;
     end
     Wlr = Wlr - eta * ( images(:, i) * (y - T(:, i))');
     lgr_error(1, i) = -1 * sum(T(:, i) - y);
@@ -52,5 +52,43 @@ end
 plot(1:length(images), lgr_error);
 
 
+% Neural net
+% number of hidden units
+j = 5;
+
+% weights for first layer d x j
+Wnn1 = zeros(d, j);
+
+% weights for first layer j x k
+Wnn2 = zeros(j, k);
+
+% bias for first layer 1 x j
+bnn1 = 0.01 * ones(1, j);
+
+% bias for first layer 1 x k
+bnn2 = 0.01 * ones(1, k);
+
+% activation function
+h = 'sigmoid';
+
+% NN gradient descent
+for i = 1 : length(images)
+    % feed forward propagation
+    z = zeros(j, 1);
+    for m = 1 : j
+            z(m, 1) = Wnn1(:,m)'* images(:,i) + bnn1(1, m);
+            z(m, 1) = sigmoid(z(m,1)); % sigmoid
+    end
+    
+    a = bsxfun(@plus, Wnn2' * z, bnn2');
+    y = zeros(k, 1);
+    exp_ak = exp(a);
+    sigma_ak = sum(exp(a));
+    for m = 1 : k
+        y(m, 1) = exp_ak(m, 1) / sigma_ak;
+    end
+    
+    
+end
 
 %save('proj3.mat');
