5029e37917396c55f57b227cf0d1891437fba0d4
nandakishore.k <nandakishorek17@gmail.com>
2015-11-29 12:33:49 -0500

    CNN using https://github.com/rasmusbergpalm/DeepLearnToolbox

---
+++ proj3_cnn.m
@@ -0,0 +1,63 @@
+%% Project 3 - Classification
+%% Nandakishore Krishna
+%% Person number : 50169797
+
+clear; close all; clc;
+
+UBitName = 'Nanda Kishore Krishna';
+personNumber = '50169797';
+
+format long g
+
+% training set
+train_x = loadMNISTImages('../data/train-images.idx3-ubyte')';
+labels = loadMNISTLabels('../data/train-labels.idx1-ubyte');
+
+% 10 digits
+k = 10;
+
+% target matrix, label 0 is mapped to 1, label 1 to 2 and so on
+train_y = zeros(k, length(labels));
+for i = 1 : k
+    train_y(i, :) = (labels == (i-1));
+end
+train_y = train_y';
+
+% validation set
+test_x = loadMNISTImages('../data/t10k-images.idx3-ubyte')';
+valLabels = loadMNISTLabels('../data/t10k-labels.idx1-ubyte');
+
+% target matrix, label 0 is mapped to 1, label 1 to 2 and so on
+test_y = zeros(k, length(valLabels));
+for i = 1 : k
+    test_y(i, :) = (valLabels == (i-1));
+end
+test_y = test_y';
+
+%% ex1 Train a 6c-2s-12c-2s Convolutional neural network 
+%will run 1 epoch in about 200 second and get around 11% error. 
+%With 100 epochs you'll get around 1.2% error
+rand('state',0)
+cnn.layers = {
+    struct('type', 'i') %input layer
+    struct('type', 'c', 'outputmaps', 6, 'kernelsize', 5) %convolution layer
+    struct('type', 's', 'scale', 2) %sub sampling layer
+    struct('type', 'c', 'outputmaps', 12, 'kernelsize', 5) %convolution layer
+    struct('type', 's', 'scale', 2) %subsampling layer
+};
+cnn = cnnsetup(cnn, train_x, train_y);
+
+opts.alpha = 1;
+opts.batchsize = 50;
+opts.numepochs = 1;
+
+cnn = cnntrain(cnn, train_x, train_y, opts);
+
+[er, bad] = cnntest(cnn, test_x, test_y);
+
+%plot mean squared error
+figure; plot(cnn.rL);
+
+assert(er<0.12, 'Too big error');
+
+save('proj3_cnn.mat');
