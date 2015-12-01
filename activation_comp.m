462078c2a65cb23be3318cc873edbbf1588c1b3d
nandakishore.k <nandakishorek17@gmail.com>
2015-12-01 12:32:45 -0500

    code for plot activation function comparison

---
+++ activation_comp.m
@@ -0,0 +1,7 @@
+h = bar([0.1296; 0.0857; 0.0861]);
+grid on
+l = cell(1,3);
+l{1}='Sigmoid'; l{2}='Relu'; l{3}='Tanh';
+set(gca,'xticklabel', l)
+set(gcf,'DefaultTextColor','red')
+ylabel('classification error', 'Color', 'r');
